4c865f7c29fee2e26b99070a85fc01129bf58dc5
Hanyoungsun1 <morrt85@gmail.com>
2022-10-13 14:35:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import {BrowserRouter, Routes, Router} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from './routes/Home'
 import Navigation from './components/Navigation'
